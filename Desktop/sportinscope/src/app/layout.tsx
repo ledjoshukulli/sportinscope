400e30138097b479b8ebdb0e54495a12e76cc7e5
ledjoshukulli <ledjoshukulli@gmail.com>
2026-09-10 13:16:40 +0200

    addsense

--- Desktop/sportinscope/src/app/layout.tsx
+++ Desktop/sportinscope/src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7172765908709117"
           crossOrigin="anonymous"
